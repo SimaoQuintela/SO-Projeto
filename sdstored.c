da6a9c924ddc84f9923668879d1754a29b389203
SimaoQuintela <simaopedroquintela@hotmail.com>
2022-04-23 17:09:34 +0100

    transformations function

--- sdstored.c
+++ sdstored.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 #include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
 #include <fcntl.h> /* O_RDONLY, O_WRONLY, O_CREAT, O_* */
 #include <string.h>
 
 #define MAX_BUFF 1024
 
+/**
+ * Dicionário com as transformações
+ */
+typedef struct config_file {
+	char* transformation;
+	int max_exec_trans;
+} config_file;
+
+
+void transformations(char* exec_args[], int num_args){
+	char *transformations[num_args-4];
+	for(int i=0; i<(num_args-4); i+=1){
+		transformations[i] = exec_args[i+4];
+		//strcpy(transformations[i], exec_args[num_args-4]);
+		printf("%s\n", transformations[i]);
+	}
+
+	/*
+	* Falta executar as transformações
+	* Marcar reunião com o grupo para fazer isto pois é a parte mais "difícil"
+	*/
+}
+
+// !!!  lembrar-me de ler o config file e meter no dicionário
 int main(int argc, char *argv[]){
 	char buffer[MAX_BUFF];
 	char* token;
@@ -33,7 +58,6 @@ int main(int argc, char *argv[]){
 	while(1){
 		j = 0;
 		i = 0;
-
 		int rd = open("main_pipe", O_RDONLY, 0666);
 		if(rd == -1){
 			perror("Erro ao abrir o pipe de leitura");
@@ -46,21 +70,28 @@ int main(int argc, char *argv[]){
 
 	 	token = strtok(buffer, " ");
         
-                while(token != NULL){
-                   exec_args[j] = token;
-        	   token = strtok(NULL, " ");
-        	   j++;
-                }
+        while(token != NULL){
+            exec_args[j] = token;
+        	token = strtok(NULL, " ");
+        	j++;
+        }
 
-                exec_args[j] = "end";
+        exec_args[j] = "\0";
+        int num_args = j;
 
-		
-		for(i=0; exec_args[i] !=  "end"; i+=1){
+        if(num_args == 2){
+        	// status()
+        } else {
+        	transformations(exec_args, num_args);
+        }
+
+		/*
+		for(i=0; exec_args[i] !=  "\0"; i+=1){
 			printf("%s\n", exec_args[i]);
 		}
+		*/
+		
 	
-
-	//	printf("Mete outra linha men\n");
 	}
 
 	unlink("main_pipe");
